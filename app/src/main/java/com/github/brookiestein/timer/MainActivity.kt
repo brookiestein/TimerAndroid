fe7b8b1f02f09b50104fb77bbfcc660c38f37103
brookiestein <this.brayan@proton.me>
2024-06-17 10:34:35 -0400

    Timer now runs as expected
    * Fixed bug that made the app to crash.
        Android views cannot be modified outside their creator thread.
    * Fixed typo on Spanish translation.

--- app/src/main/java/com/github/brookiestein/timer/MainActivity.kt
+++ app/src/main/java/com/github/brookiestein/timer/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.github.brookiestein.timer
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -8,88 +7,74 @@ import android.widget.NumberPicker
 import android.widget.TextView
 import android.widget.Toast
 import java.util.Calendar
+import kotlin.concurrent.thread
 
 class Run(
-    private var context: Context? = null,
-    private var hoursPicker: NumberPicker? = null,
-    private var minutesPicker: NumberPicker? = null,
-    private var secondsPicker: NumberPicker? = null,
-    private var startButton: Button? = null,
-    private var setEnabled: (Boolean) -> Unit
+    private val hoursPicker: NumberPicker,
+    private val minutesPicker: NumberPicker,
+    private val secondsPicker: NumberPicker,
+    private val setStatusText: (Boolean, Int, Int, Int) -> Unit
 ) : Runnable {
     private var running = false
+    private var end = false
+    private var hasBeenStarted = false
+    private var hours = hoursPicker.value
+    private var minutes = minutesPicker.value
+    private var seconds = secondsPicker.value
 
     fun start() {
+        if (hasBeenStarted) {
+            return
+        }
         running = true
+        end = false
+        hasBeenStarted = true
+        setStatusText(false, hours, minutes, seconds)
     }
 
-    fun stop() {
+    fun pause() {
         running = false
     }
 
-    fun setContext(ctx: Context) {
-        context = ctx
+    fun stop() {
+        end = true
+        running = false
+        hoursPicker.value = 0
+        minutesPicker.value = 0
+        secondsPicker.value = 0
+        setStatusText(true, 0, 0, 0)
     }
 
-    fun setHoursPicker(hp: NumberPicker) {
-        hoursPicker = hp
-    }
-
-    fun setMinutesPicker(mp: NumberPicker) {
-        minutesPicker = mp
-    }
-
-    fun setSecondsPicker(sp: NumberPicker) {
-        secondsPicker = sp
-    }
-
-    fun setStartButton(sb: Button) {
-        startButton = sb
-    }
-
-    fun setCallback(callback: (Boolean) -> Unit) {
-        setEnabled = callback
-    }
+    fun isRunning() = running
+    fun atEnd() = end
 
     override fun run() {
-        var hours = hoursPicker?.value
-        var minutes = minutesPicker?.value
-        var seconds = secondsPicker?.value
-
         while (running) {
             if (seconds == 0) {
-                if (minutes != null) {
-                    if (minutes > 0) {
-                        --minutes
-                        minutesPicker?.value = minutes
+                if (minutes > 0) {
+                    --minutes
+                    seconds = 60
+                } else {
+                    if (hours > 0) {
+                        --hours
+                        minutes = 59
                         seconds = 60
-                    } else {
-                        if (hours != null) {
-                            if (hours > 0) {
-                                --hours
-                                hoursPicker?.value = hours
-                                minutes = 59
-                                minutesPicker?.value = minutes
-                                seconds = 60
-                            }
-                        }
                     }
                 }
             }
 
-            seconds = seconds!! - 1
-            secondsPicker?.value = seconds
+            seconds -= 1
+            hoursPicker.value = hours
+            minutesPicker.value = minutes
+            secondsPicker.value = seconds
 
             if (hours == 0 && minutes == 0 && seconds == 0) {
+                stop()
                 break
             }
 
             Thread.sleep(1000)
         }
-
-        /* FIXME: This makes the app to crash */
-        startButton?.text = context?.getString(R.string.start)
-        setEnabled(true)
     }
 }
 
@@ -114,9 +99,14 @@ class MainActivity : AppCompatActivity() {
         secondsPicker.maxValue = 59
         secondsPicker.value = 0
 
+        val setEnabled: (Boolean) -> Unit = { enabled ->
+            hoursPicker.isEnabled = enabled
+            minutesPicker.isEnabled = enabled
+            secondsPicker.isEnabled = enabled
+        }
+
         val statusText: TextView = findViewById(R.id.statusTextView)
         val setStatusText: (Boolean, Int, Int, Int) -> Unit = { clear, h, m, s ->
-
             var text: String
 
             if (clear) {
@@ -139,7 +129,7 @@ class MainActivity : AppCompatActivity() {
                         --minutes
                     }
 
-                    if (hour >= 12) {
+                    while (hour >= 12) {
                         hour -= 12
                         indicator = if (indicator == Calendar.AM) {
                             Calendar.PM
@@ -170,16 +160,14 @@ class MainActivity : AppCompatActivity() {
             statusText.text = text
         }
 
-        val setEnabled: (Boolean) -> Unit = { enabled ->
-            hoursPicker.isEnabled = enabled
-            minutesPicker.isEnabled = enabled
-            secondsPicker.isEnabled = enabled
-        }
-
         val startButton: Button = findViewById(R.id.startButton)
         val pauseButton: Button = findViewById(R.id.pauseButton)
 
-        val runner = Run(setEnabled = setEnabled)
+        var runner = Run(
+            hoursPicker, minutesPicker, secondsPicker,
+            setStatusText
+        )
+
         var thread: Thread
 
         startButton.setOnClickListener {
@@ -191,34 +179,42 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(
                     this,
                     getString(R.string.allZeroes),
-                    Toast.LENGTH_LONG
+                    Toast.LENGTH_SHORT
                 ).show()
                 return@setOnClickListener
             }
 
             if (startButton.text == getString(R.string.stop)) {
-                setEnabled(true)
                 startButton.text = getString(R.string.start)
                 runner.stop()
-                hoursPicker.value = 0
-                minutesPicker.value = 0
-                secondsPicker.value = 0
-                setStatusText(true, 0, 0, 0)
                 return@setOnClickListener
             }
 
-            runner.setContext(this)
-            runner.setHoursPicker(hoursPicker)
-            runner.setMinutesPicker(minutesPicker)
-            runner.setSecondsPicker(secondsPicker)
-            runner.setStartButton(startButton)
-            runner.setCallback(setEnabled)
+            /* Make new thread every time user starts new timer because
+             * threads cannot be restarted.
+             */
+            runner = Run(
+                hoursPicker, minutesPicker, secondsPicker,
+                setStatusText
+            )
             runner.start()
             thread = Thread(runner)
             thread.start()
             startButton.text = getString(R.string.stop)
+            pauseButton.text = getString(R.string.pause)
             setEnabled(false)
-            setStatusText(false, hours, minutes, seconds)
+
+            val checkForTimerFinished = thread(false) {
+                while (!runner.atEnd()) {
+                    Thread.sleep(1000)
+                }
+
+                runOnUiThread {
+                    startButton.text = getString(R.string.start)
+                    setEnabled(true)
+                }
+            }
+            checkForTimerFinished.start()
         }
 
         pauseButton.setOnClickListener {
@@ -231,30 +227,35 @@ class MainActivity : AppCompatActivity() {
                     .makeText(
                         this,
                         getString(R.string.noNeedToPause),
-                        Toast.LENGTH_LONG
+                        Toast.LENGTH_SHORT
                     ).show()
                 return@setOnClickListener
             }
 
             if (pauseButton.text == getString(R.string.pause)) {
-                runner.stop()
+                runner.pause()
                 pauseButton.text = getString(R.string.resume)
                 Toast
                     .makeText(
                         this,
                         getString(R.string.timerPaused),
-                        Toast.LENGTH_LONG
+                        Toast.LENGTH_SHORT
                     ).show()
             } else {
+                runner = Run(
+                    hoursPicker, minutesPicker, secondsPicker,
+                    setStatusText
+                )
                 runner.start()
                 thread = Thread(runner)
                 thread.start()
                 pauseButton.text = getString(R.string.pause)
+
                 Toast
                     .makeText(
                         this,
                         getString(R.string.timerResumed),
-                        Toast.LENGTH_LONG
+                        Toast.LENGTH_SHORT
                     ).show()
             }
         }
